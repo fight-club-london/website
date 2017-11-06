317856fff01bc1a62abbbcb6ace393bc244c2f1e
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-11-06 14:16:11 +0000

    comments out component helpers because we have no components yet

--- lib/ev2_web/views/component_helpers.ex
+++ lib/ev2_web/views/component_helpers.ex
@@ -1,7 +1,7 @@
 defmodule Ev2.ComponentHelpers do
-  def component(template, assigns \\ []) do
-    Ev2.ComponentView.render(template, assigns)
-  end
+  # def component(template, assigns \\ []) do
+  #   Ev2.ComponentView.render(template, assigns)
+  # end
 
   # def component(template, assigns, do: block) do
   #   Ev2.ComponentView.render(template, Keyword.merge(assigns, [do: block]))
