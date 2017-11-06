0dfd151a5cb49638c120ac1959f8fb0d6ef8f4e4
Jack Murphy <jmurphy.web@gmail.com>
2017-11-06 10:20:13 +0000

    #1 - remove cover alias

--- mix.exs
+++ mix.exs
@@ -63,8 +63,7 @@ defmodule Ev2.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"],
-      "cover": ["coveralls", "coveralls.html"]
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
