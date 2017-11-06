0cc87210c37dec0caf6b6bbcee2884a70cfe3893
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-11-06 15:13:28 +0000

    updates docusign production config

--- config/prod.exs
+++ config/prod.exs
@@ -32,7 +32,7 @@ config :ev2, Ev2.Repo,
   ssl: true
 
 # Configure Docusign
-config :engine, Engine.Sign,
+config :ev2, Engine.Sign,
   username: System.get_env("DOCUSIGN_USERNAME"),
   password: System.get_env("DOCUSIGN_PASSWORD"),
   integrator_key: System.get_env("DOCUSIGN_INTEGRATOR_KEY"),
