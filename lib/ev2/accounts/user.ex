1dc81e2aef5f4d0fe707d2c37058c148f1e6f8d5
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-11-07 12:52:37 +0000

    adds assets to the project

--- lib/ev2/accounts/user.ex
+++ lib/ev2/accounts/user.ex
@@ -2,7 +2,7 @@ defmodule Ev2.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
   alias Ev2.Accounts.{User, Role}
-
+  alias Comeonin.Bcrypt
 
   schema "users" do
     field :active, :boolean, nil: false, default: true
@@ -40,6 +40,46 @@ defmodule Ev2.Accounts.User do
       :terms_accepted,
       :active
     ])
+    |> email_changeset(attrs)
+  end
+
+  def email_changeset(struct, attrs \\ %{}) do
+    struct
+    |> cast(attrs, [:email])
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
     |> unique_constraint(:email)
+  end
+
+  def registration_changeset(struct, attrs \\ %{}) do
+    struct
+    |> changeset(attrs)
+    |> terms_accepted(attrs)
+    |> validate_password()
+    |> put_password_hash()
+  end
+
+  defp terms_accepted(changeset, attrs) do
+    message = "You must agree to the terms and conditions"
+    changeset
+    |> cast(attrs, [:terms_accepted])
+    |> validate_required([:terms_accepted])
+    |> validate_inclusion(:terms_accepted, [true], message: message)
+  end
+
+  defp validate_password(changeset) do
+    changeset
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_format(:password, ~r/^(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/)
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: given_pass}} ->
+        put_change(changeset, :password_hash, Bcrypt.hashpwsalt(given_pass))
+      _ ->
+        changeset
+    end
   end
 end
