5fc849379ef3e9b74665841fda06caa269844275
Jack Murphy <jmurphy.web@gmail.com>
2017-11-07 11:07:08 +0000

    #2 - run roles permissions and role_permissions generators

---
+++ priv/repo/migrations/20171107084759_create_role_permissions.exs
@@ -0,0 +1,16 @@
+defmodule Ev2.Repo.Migrations.CreateRolePermissions do
+  use Ecto.Migration
+
+  def change do
+    create table(:role_permissions) do
+      add :role_id, references(:roles, on_delete: :nothing)
+      add :permission_id, references(:permissions, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:role_permissions, [:role_id])
+    create index(:role_permissions, [:permission_id])
+    create unique_index(:role_permissions, [:role_id, :permission_id], name: :unique_role_permissions)
+  end
+end
