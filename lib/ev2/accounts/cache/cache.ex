906ea0b1c9aaa978eb919149b60363fe78b990c8
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-11-08 12:38:25 +0000

    adds more tests

--- lib/ev2/accounts/cache/cache.ex
+++ lib/ev2/accounts/cache/cache.ex
@@ -8,11 +8,11 @@ defmodule Ev2.Accounts.Cache do
 
   def query(param), do: Redix.command(:redix, param)
 
-  def get(key), do: query(["get", key])
-  def set(key, value), do: query(["set", key, value])
-  def expire(key, seconds), do: query(["expire", key, seconds])
-  def flushdb do
-    query(["flushdb"])
-    :ok
-  end
+  # def get(key), do: query(["get", key])
+  # def set(key, value), do: query(["set", key, value])
+  # def expire(key, seconds), do: query(["expire", key, seconds])
+  # def flushdb do
+  #   query(["flushdb"])
+  #   :ok
+  # end
 end
