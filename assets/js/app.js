4fd4338ae6f93c449a47bf135dfe3dab32269cb0
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2018-01-02 13:58:16 +0000

    fixes timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,50 +20,54 @@ import "phoenix_html"
 
 // import socket from "./socket"
 function timerListeners(day, month, year, id) {
-  setInterval(function () {
-    var eventDate = new Date(year, month - 1, day, 20).getTime();
+  var timer = setInterval(function () {
+    var eventDate = new Date(year, month - 1, day, 13, 58).getTime();
     var today = new Date().getTime();
-    // seconds
     var eventSeconds = Math.round(eventDate / 1000);
     var todaySeconds = Math.round(today / 1000);
-    var secondsOut = eventSeconds - todaySeconds;
-    var roundedSeconds = secondsOut % 60;
-    var secondContainer = document.getElementById('seconds' + id);
-    var secondTextContainer = document.getElementById('seconds-text' + id);
-    secondContainer.innerHTML = zeroPad(roundedSeconds);
+    if (eventSeconds - todaySeconds < 0) {
+      clearInterval(timer);
+    } else {
+      // seconds
+      var secondsOut = eventSeconds - todaySeconds;
+      var roundedSeconds = secondsOut % 60;
+      var secondContainer = document.getElementById('seconds' + id);
+      var secondTextContainer = document.getElementById('seconds-text' + id);
+      secondContainer.innerHTML = zeroPad(roundedSeconds);
 
-    // minutes
-    var eventMinutes = eventSeconds / 60;
-    var todayMinutes = todaySeconds / 60;
-    var minutesOut = eventMinutes - todayMinutes;
-    var roundedMinutes = Math.floor(minutesOut % 60);
-    var minuteContainer = document.getElementById('minutes' + id);
-    var minuteTextContainer = document.getElementById('minutes-text' + id);
-    minuteContainer.innerHTML = zeroPad(roundedMinutes);
-    if (roundedDays === 1) {
-      minuteTextContainer.innerHTML = "minute";
-    }
-    // hours
-    var eventHours = eventMinutes / 60;
-    var todayHours = todayMinutes / 60;
-    var hoursOut = eventHours - todayHours;
-    var roundedHours = Math.floor(hoursOut % 24);
-    var hourContainer = document.getElementById('hours' + id);
-    var hourTextContainer = document.getElementById('hours-text' + id);
-    hourContainer.innerHTML = zeroPad(roundedHours);
-    if (roundedHours === 1) {
-      hourTextContainer.innerHTML = "hour";
-    }
-    // days
-    var eventDays = eventHours / 24;
-    var todayDays = todayHours / 24;
-    var daysOut = (eventDays - todayDays);
-    var roundedDays = Math.floor(daysOut % 365);
-    var dayContainer = document.getElementById('days' + id);
-    var dayTextContainer = document.getElementById('days-text' + id);
-    dayContainer.innerHTML = zeroPad(roundedDays);
-    if (roundedDays === 1) {
-      dayTextContainer.innerHTML = "day";
+      // minutes
+      var eventMinutes = eventSeconds / 60;
+      var todayMinutes = todaySeconds / 60;
+      var minutesOut = eventMinutes - todayMinutes;
+      var roundedMinutes = Math.floor(minutesOut % 60);
+      var minuteContainer = document.getElementById('minutes' + id);
+      var minuteTextContainer = document.getElementById('minutes-text' + id);
+      minuteContainer.innerHTML = zeroPad(roundedMinutes);
+      if (roundedDays === 1) {
+        minuteTextContainer.innerHTML = "minute";
+      }
+      // hours
+      var eventHours = eventMinutes / 60;
+      var todayHours = todayMinutes / 60;
+      var hoursOut = eventHours - todayHours;
+      var roundedHours = Math.floor(hoursOut % 24);
+      var hourContainer = document.getElementById('hours' + id);
+      var hourTextContainer = document.getElementById('hours-text' + id);
+      hourContainer.innerHTML = zeroPad(roundedHours);
+      if (roundedHours === 1) {
+        hourTextContainer.innerHTML = "hour";
+      }
+      // days
+      var eventDays = eventHours / 24;
+      var todayDays = todayHours / 24;
+      var daysOut = (eventDays - todayDays);
+      var roundedDays = Math.floor(daysOut % 365);
+      var dayContainer = document.getElementById('days' + id);
+      var dayTextContainer = document.getElementById('days-text' + id);
+      dayContainer.innerHTML = zeroPad(roundedDays);
+      if (roundedDays === 1) {
+        dayTextContainer.innerHTML = "day";
+      }
     }
   }, 1000)
 }
