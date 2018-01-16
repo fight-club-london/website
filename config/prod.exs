3aa8e9e96369b60d77b5480e367296699cf7f772
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2018-01-16 15:51:36 +0000

    forces ssl

--- config/prod.exs
+++ config/prod.exs
@@ -16,10 +16,11 @@ use Mix.Config
 config :lfc, LfcWeb.Endpoint,
   load_from_system_env: true,
   url: [
-    scheme: "http",
+    scheme: "https",
     host: "fightclublondon.herokuapp.com",
     port: 443
     ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
